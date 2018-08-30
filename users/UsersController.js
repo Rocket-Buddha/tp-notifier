4d369f3b1fca3850bbcdab7d457281bc8379b36f
Eduardo David Angeleri <angeleridavid@gmail.com>
2018-08-29 21:41:24 -0300

    On branch task-#1  Changes to be committed: 	modified:   app.js 	new file:   db.js 	new file:   package-lock.json 	modified:   package.json 	deleted:    routes/index.js 	deleted:    routes/users.js 	new file:   server.js 	new file:   users/UserModel.js 	new file:   users/UsersController.js 	deleted:    views/error.ejs 	deleted:    views/index.ejs

---
+++ users/UsersController.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
+var User = require('./UserModel');
+
+// CREATES A NEW USER
+router.post('/', function (req, res) {
+    User.create({
+            name : req.body.name,
+            email : req.body.email,
+            password : req.body.password
+        }, 
+        function (err, user) {
+            if (err) return res.status(500).send("There was a problem adding the information to the database.");
+            res.status(200).send(user);
+        });
+});
+
+router.get('/', function(req, res){
+  res.send("GET llego");
+});
+
+module.exports = router;
